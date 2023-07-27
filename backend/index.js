40e7300fea4a684c0a5381532ddc12bb61a10bd1
andresdlRoca <andresdelaroca11@gmail.com>
2023-07-26 19:02:18 -0600

    Added basic logging in with existing users and initial menu

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,29 @@ const serverData = {
 };
 
 let userCredentials = {
-    username: 'testeo20332',
-    password: '123456789'
+    username: 'andres20332',
+    password: 'andres20332',
+    userWithDomain: 'andres20332@alumchat.xyz'
 };
 
 const client = new net.Socket();
+let loggenIn = false;
+
+const showMenu = () => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    console.log(`\nCurrent User: ${userCredentials.userWithDomain}`)
+    console.log('\nMenu:');
+    console.log('1. Send Message');
+    console.log()
+
+}
 
 
+//TODO: Add a way to register a new user
 function connectToServer() {
     client.connect(serverData.port, serverData.domain, () => {
         console.log('Connected');
@@ -24,12 +40,48 @@ function connectToServer() {
     });
 
     client.on('data', data=> {
-        console.log(data.toString());
+        sendCredentials(client, data);
     });
+
+    client.on('close', () => {
+        console.log('Connection closed');
+    });
+
+    client.on('error', (err) => {
+        console.log('Error: ', err);
+    });
+
+    client.on('end', () => {
+        console.log('Connection ended');
+    });
+
 };
 
-function sendCredentials() {
-    client.write(`<auth xmlns="urn:ietf:params:xml:ns:xmpp-sasl" mechanism="PLAIN">${Buffer.from(`\u0000${userCredentials.username}\u0000${userCredentials.password}`).toString('base64')}</auth>`);
+function sendCredentials(client, data) {
+    const incomingData = data.toString();
+
+    if(incomingData.includes('stream:features') && !loggenIn) {
+        console.log("Requesting access...")
+        // console.log(incomingData);
+        requestAccess(client);
+    } else if(incomingData.includes('success')) {
+        console.log('Logged in');
+        // client.write('</stream:stream>');
+        loggenIn = true;
+        showMenu();
+        // client.write(`<stream:stream to="${serverData.domain}" xmlns="jabber:client" xmlns:stream="http://etherx.jabber.org/streams" version="1.0">`);
+    } else if(incomingData.includes('failure')){
+        console.log('Wrong credentials, Authentication failed');
+        console.log('Error: ', incomingData);
+    } else { //TODO: Add a way to manage first message when logging in.
+        console.log('Something went wrong');
+        console.log('Response: ', incomingData);
+
+    }
+};
+
+async function requestAccess(client) {
+    Promise.resolve(client.write(`<auth xmlns="urn:ietf:params:xml:ns:xmpp-sasl" mechanism="PLAIN">${Buffer.from(`\u0000${userCredentials.username}\u0000${userCredentials.password}`).toString('base64')}</auth>`));
 };
 
 connectToServer();
